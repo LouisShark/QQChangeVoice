93eb6aaf1d6ce12cfd4e2e2b1ac050abb831f326
LouisShark <13185137187@163.com>
2017-08-25 15:46:31 +0800

    commit :hand:

--- app/src/main/java/cn/louis/shark/qqvoice/MainActivity.java
+++ app/src/main/java/cn/louis/shark/qqvoice/MainActivity.java
@@ -10,17 +10,48 @@ import android.widget.Toast;
 import org.fmod.FMOD;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separatorChar + "louis.wav";
 
+    private InputStream inputStream = null;
+    private FileOutputStream outputStream = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FMOD.init(this);
+        File oldFile = new File(path);
+        if (!oldFile.exists()) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int bytesum = 0;
+                        int byteread = 0;
+                        File file = new File(path);
+                        inputStream = getResources().getAssets().open("singing.wav");
+                        outputStream = new FileOutputStream(file);
+                        byte[] buffer = new byte[1444];
+                        while ((byteread = inputStream.read(buffer)) != -1) {
+                            bytesum += byteread;
+                            System.out.println(bytesum);
+                            outputStream.write(buffer, 0, byteread);
+                        }
+                        inputStream.close();
+                    } catch (Exception e) {
+                        System.out.println("复制单个文件操作出错");
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+
     }
 
     @Override
