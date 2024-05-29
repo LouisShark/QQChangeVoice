7416bc905509386b6ac9237f2e6ca1c84dfe2727
LouisShark <mshark.louis@gmail.com>
2024-05-29 17:57:42 +0800

    Implement save sound with effect to SD card feature

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 利用fmod完成qq的变声功能
 
 **因为只是我学习时的demo 所以只完成了功能，性能之类的东西并没有考虑，如果要使用请自己完成线程之类的编码**
+
+## Save Sound with Effect
+To save a sound with an effect applied to it, use the `saveSoundWithEffect` method in the `VoiceFixer` class. This method allows you to specify the input sound file, the output path where the sound file will be saved, and the effect mode to apply. Ensure you have the necessary permissions to write to the SD card.
--- app/src/main/java/cn/louis/shark/qqvoice/VoiceFixer.java
+++ app/src/main/java/cn/louis/shark/qqvoice/VoiceFixer.java
@@ -1,5 +1,10 @@
 package cn.louis.shark.qqvoice;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by LouisShark on 2017/8/24.
  * this is on cn.louis.shark.qqvoice.
@@ -15,7 +20,32 @@ public class VoiceFixer {
 
     public static native void fix(String path, int mode);
 
+    /**
+     * Saves the sound with effect applied to a specified path on the SD card.
+     * 
+     * @param inputPath The path of the input sound file.
+     * @param outputPath The path where the output sound file will be saved.
+     * @param mode The effect mode to apply.
+     */
+    public static void saveSoundWithEffect(String inputPath, String outputPath, int mode) {
+        // Apply the effect using the native fix method
+        fix(inputPath, mode);
 
+        // Perform file I/O operations to save the processed sound
+        File inputFile = new File(inputPath);
+        File outputFile = new File(outputPath);
+
+        try (FileInputStream fis = new FileInputStream(inputFile);
+             FileOutputStream fos = new FileOutputStream(outputFile)) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                fos.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     static {
         System.loadLibrary("fmod");
